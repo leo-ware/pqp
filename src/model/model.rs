f393de18ac6653b1796b308368fc97506165fa2f
Leo Ware <leoware@uni.minerva.edu>
2022-09-18 14:30:40 -0300

    bad old model

--- src/model/model.rs
+++ src/model/model.rs
@@ -1,5 +1,9 @@
 use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
 use unordered_pair::UnorderedPair;
+use cute::c;
+
+use crate::form::Form;
 
 #[derive(Debug, PartialEq, Eq)]
 #[allow(dead_code)]
@@ -34,18 +38,15 @@ impl<'a> Model<'a> {
         return Model {dag: dag_map, confounded: confounded_set, vars: vars_set};
     }
 
-    pub fn ancestors_block (&self, x: &'a str, block: Vec<&str>) -> HashSet<&str> {
+    pub fn ancestors_block (&self, x: HashSet<&'a str>, block: HashSet<&str>) -> HashSet<&'a str> {
 
         let mut a = HashSet::new();
         let mut acc = Vec::new();
 
-        let mut block_set = HashSet::new();
-        for blocked in block {
-            block_set.insert(blocked);
-        }
-
-        if !block_set.contains(x) {
-            acc.push(x);
+        for var in x {
+            if !block.contains(var) {
+                acc.push(var);
+            }
         }
         
         loop {
@@ -58,7 +59,7 @@ impl<'a> Model<'a> {
                             for parent in parents.iter() {
                                 if !a.contains(parent) {
                                     a.insert(*parent);
-                                    if !block_set.contains(*parent) {
+                                    if !block.contains(*parent) {
                                         acc.push(*parent);
                                     }
                                 }
@@ -72,8 +73,8 @@ impl<'a> Model<'a> {
         return a;
     }
 
-    pub fn ancestors (&self, x: &'a str) -> HashSet<&str> {
-        return self.ancestors_block(x, Vec::new());
+    pub fn ancestors (&self, x: HashSet<&'a str>) -> HashSet<&'a str> {
+        return self.ancestors_block(x, HashSet::new());
     }
 
     pub fn c_components (&self) -> Vec<HashSet<&str>> {
@@ -162,6 +163,43 @@ impl<'a> Model<'a> {
         }
 
         return Model {dag, confounded, vars};
+    }
+
+    pub fn id (&self, y: HashSet<&str>, x: HashSet<&str>) -> Form {
+        // step 1
+        if x.is_empty() {
+            return Form::Sum(
+                HashSet::from_iter(self.vars.difference(&y).map(|e| *e)),
+                Box::new(Form::P(c![*x, for x in y.iter()], None)),
+            );
+        }
+
+        // step 2
+        let a_y = self.ancestors(y);
+        for var in self.vars.iter() {
+            if !a_y.contains(*var) {
+                return self.subgraph(a_y).id(y, HashSet::from_iter(x.union(&a_y).map(|e| *e)));
+            }
+        }
+
+        // step 3
+        let a_y_c = self.ancestors_block(y, x);
+        let mut w = self.vars.clone();
+        w.retain(|e| !a_y_c.contains(*e) && !x.contains(*e));
+        if !w.is_empty() {
+            return self.id(y, HashSet::from_iter(x.union(&w).map(|e| *e)));
+        }
+
+        // step 4
+        let c_components = self.c_components();
+        if c_components.len() > 1 {
+            return Form::Sum(
+                HashSet::from_iter(iter),
+                ()
+            );
+        }
+
+        return None;
     }
 
 }
