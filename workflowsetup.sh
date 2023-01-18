0106b45fe46c0c1555479bfec0b1ca91cbb21d67
Leo Ware <leoware@uni.minerva.edu>
2023-01-18 11:02:44 +0800

    gahhhh

--- workflowsetup.sh
+++ workflowsetup.sh
@@ -1,4 +1,6 @@
 python3 -m venv venv
 source venv/bin/activate
 pip install -q -r sherlock_py/requirements.txt
-maturin develop -q -m backend/Cargo.toml
+cd backend
+# maturin develop -q -m backend/Cargo.toml
+maturin develop
