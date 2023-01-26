54465d7e2dba38dde43d7f3e63ffc8c83e969130
Leo Ware <leoware@uni.minerva.edu>
2023-01-26 15:21:34 +0800

    deploy

--- python/pqp/docs/source/index.rst
+++ python/pqp/docs/source/index.rst
@@ -6,7 +6,7 @@ Welcome to ``pqp``'s documentation!
 
 The underlying Rust library, ``pqp``, was designed for performant causal identification, with a focus on correctness. ``pqp`` leverages this capability by providing performant identification routines that can handle large graphs. The package is intended to be a tool for academic research and data analysis, and we hope it can aid in the advancement of knowledge in the field of causality.
 
-**Please Note:** This package is still in development, and the API is subject to change. Additionally, not all features may work as advertised. We welcome any feedback or suggestions for improvement.
+**Please Note:** This package is in development, and the API is subject to change. Additionally, not all features may work as advertised. We welcome any feedback or suggestions for improvement.
 
 
 Getting Started
--- sphinx.sh
+++ sphinx.sh
@@ -2,27 +2,28 @@
 
 SOURCEDIR="python/pqp/docs/source"
 BUILDDIR="python/pqp/docs/build"
-PACKAGEDIR="python/pqp"
+# PACKAGEDIR="python/pqp"
 
 if [ "$1" = "build" ]; then
     sphinx-build $SOURCEDIR $BUILDDIR
-# elif [$1 -eq "livehtml"]; then
-#     sphinx-autobuild "$(SOURCEDIR)" "$(BUILDDIR)"
-# elif [$1 -eq "autodoc"]; then
-#     sphinx-apidoc -f -o "$(SOURCEDIR)" "$(PACKAGEDIR)"
-# elif [$1 -eq "buildpush"]; then
-#     cd "$(BUILDDIR)"
-#     git init
-#     git add .
-#     git commit -m "deploy"
+elif [ "$1" = "livehtml" ]; then
+    sphinx-autobuild "$(SOURCEDIR)" "$(BUILDDIR)"
+elif [ "$1" = "autodoc" ]; then
+    sphinx-apidoc -f -o "$(SOURCEDIR)" "$(PACKAGEDIR)"
+elif [ "$1" = "deploy" ]; then
+    cd "$(BUILDDIR)" || exit
+    git init
+    git add .
+    git commit -m "deploy"
 
-#     git remote add origin https://github.com/leo-ware/casptone-docs.git
-#     git branch -M main
-#     git push -u origin main
+    git branch -M gh-pages
+    git remote add origin https://github.com/leo-ware/capstone
+    git pull origin gh-pages -s ours --allow-unrelated-histories --no-edit
+    git push -u origin gh-pages
 
-#     git rm -fr .git
-# elif [$1 -eq ""]; then
-#     echo "No argument provided. Valid arguments are: build, livehtml, autodoc"
+    rm -f -r .git
+elif [ "$1" = "" ]; then
+    echo "No argument provided. Valid arguments are: build, livehtml, autodoc"
 else
     echo "Invalid argument. Valid arguments are: build, livehtml, autodoc, buildpush"
 fi
